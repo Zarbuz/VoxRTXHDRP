389630abac7061331de16661c0ea7378e95bf2f8
Nicolas Perrier <nperrier@manzalab.com>
2022-11-29 19:24:24 +0100

    Fixed invalid offset when lod level is > 1

--- Assets/VoxToVFXFramework/Scripts/Jobs/ComputePathTracingDataJob.cs
+++ Assets/VoxToVFXFramework/Scripts/Jobs/ComputePathTracingDataJob.cs
@@ -33,31 +33,32 @@ namespace VoxToVFXFramework.Scripts.Jobs
 			}
 			else
 			{
+				float offset = 0.5f * chunk.LodLevel;
 				if ((additionalData.VoxelFace & VoxelFace.Top) != 0)
 				{
 					Matrix4x4 matrix = new Matrix4x4();
-					matrix.SetTRS(worldPosition + new Vector3(0, 0.5f, 0), Quaternion.Euler(90, 0, 0), Vector3.one * chunk.LodLevel);
+					matrix.SetTRS(worldPosition + new Vector3(0, offset, 0), Quaternion.Euler(90, 0, 0), Vector3.one * chunk.LodLevel);
 					Result.Add(colorIndex, matrix);
 				}
 
 				if ((additionalData.VoxelFace & VoxelFace.Right) != 0)
 				{
 					Matrix4x4 matrix = new Matrix4x4();
-					matrix.SetTRS(worldPosition + new Vector3(0.5f, 0, 0), Quaternion.Euler(0, -90, 0), Vector3.one * chunk.LodLevel);
+					matrix.SetTRS(worldPosition + new Vector3(offset, 0, 0), Quaternion.Euler(0, -90, 0), Vector3.one * chunk.LodLevel);
 					Result.Add(colorIndex, matrix);
 				}
 
 				if ((additionalData.VoxelFace & VoxelFace.Bottom) != 0)
 				{
 					Matrix4x4 matrix = new Matrix4x4();
-					matrix.SetTRS(worldPosition + new Vector3(0, -0.5f, 0), Quaternion.Euler(270, 0, 0), Vector3.one * chunk.LodLevel);
+					matrix.SetTRS(worldPosition + new Vector3(0, -offset, 0), Quaternion.Euler(270, 0, 0), Vector3.one * chunk.LodLevel);
 					Result.Add(colorIndex, matrix);
 				}
 
 				if ((additionalData.VoxelFace & VoxelFace.Left) != 0)
 				{
 					Matrix4x4 matrix = new Matrix4x4();
-					matrix.SetTRS(worldPosition + new Vector3(-0.5f, 0, 0), Quaternion.Euler(0, 90, 0), Vector3.one * chunk.LodLevel);
+					matrix.SetTRS(worldPosition + new Vector3(-offset, 0, 0), Quaternion.Euler(0, 90, 0), Vector3.one * chunk.LodLevel);
 					Result.Add(colorIndex, matrix);
 
 				}
@@ -65,14 +66,14 @@ namespace VoxToVFXFramework.Scripts.Jobs
 				if ((additionalData.VoxelFace & VoxelFace.Front) != 0)
 				{
 					Matrix4x4 matrix = new Matrix4x4();
-					matrix.SetTRS(worldPosition + new Vector3(0, 0, 0.5f), Quaternion.Euler(0, 180, 0), Vector3.one * chunk.LodLevel);
+					matrix.SetTRS(worldPosition + new Vector3(0, 0, offset), Quaternion.Euler(0, 180, 0), Vector3.one * chunk.LodLevel);
 					Result.Add(colorIndex, matrix);
 				}
 
 				if ((additionalData.VoxelFace & VoxelFace.Back) != 0)
 				{
 					Matrix4x4 matrix = new Matrix4x4();
-					matrix.SetTRS(worldPosition + new Vector3(0, 0, -0.5f), Quaternion.Euler(0, 0, 0), Vector3.one * chunk.LodLevel);
+					matrix.SetTRS(worldPosition + new Vector3(0, 0, -offset), Quaternion.Euler(0, 0, 0), Vector3.one * chunk.LodLevel);
 					Result.Add(colorIndex, matrix);
 				}
 			}
